0239e8bc2106f102dec294b1f8d194f06424cbe7
aditya-purang <44022838+aditya-purang@users.noreply.github.com>
2021-02-18 18:08:52 +0530

    Create boombot.py

---
+++ boombot.py
@@ -0,0 +1,34 @@
+
+from flask import Flask,jsonify
+from youtubesearchpython import VideosSearch
+import json
+
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return "api"
+
+@app.route('/getlinks/<string:song_str>', methods=['GET'])
+def get_links(song_str):
+    songlist=song_str.split(',')
+    for i in range(len(songlist)):
+        songlist[i]=songlist[i].strip()
+
+    url_list=[]
+    videosSearch = VideosSearch(songlist[0], limit = 1)
+
+    url_list=[VideosSearch(song, limit = 1).result().get("result")[0].get("link") for song in songlist]
+
+    return jsonify(url_list)
+
+
+app.run()
+
+
+
+   
+
