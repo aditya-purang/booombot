159a45a2005357fc57cb35343197722d670b7579
aditya-purang <44022838+aditya-purang@users.noreply.github.com>
2021-02-20 16:34:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 
-from flask import Flask,jsonify
-from youtubesearchpython import VideosSearch,CustomSearch,VideoSortOrder
+from flask import Flask, jsonify
+from youtubesearchpython import VideosSearch, CustomSearch, VideoSortOrder
 
 
 app = Flask(__name__)
@@ -13,38 +13,39 @@ title: "Ashes on Fire",
       img_src: "./images/song-3.jpg",
       src:
 '''
+
+
 def return_dict(song_name):
 
-    song_dict=CustomSearch(song_name, VideoSortOrder.viewCount, limit = 1).result()
-    src=song_dict.get("result")[0].get("link")
-    image_src=song_dict.get("result")[0].get("thumbnails")[0].get("url")
-    channel=song_dict.get("result")[0].get("channel").get("name")
-    title=song_dict.get("result")[0].get("title")
+    song_dict = CustomSearch(
+        song_name, VideoSortOrder.viewCount, limit=1).result()
+    src = song_dict.get("result")[0].get("link")
+    image_src = song_dict.get("result")[0].get("thumbnails")[0].get("url")
+    channel = song_dict.get("result")[0].get("channel").get("name")
+    title = song_dict.get("result")[0].get("title")
 
-    ret_dict={"src":src,"image_src":image_src,"channel":channel,"title":title}
+    ret_dict = {"src": src, "image_src": image_src,
+                "channel": channel, "title": title}
     return ret_dict
+
 
 @app.route('/', methods=['GET'])
 def home():
     return "api"
 
+
 @app.route('/getlinks/<string:song_str>', methods=['GET'])
 def get_links(song_str):
-    songlist=song_str.split(',')
+    songlist = song_str.split(',')
     for i in range(len(songlist)):
-        songlist[i]=songlist[i].strip()
+        songlist[i] = songlist[i].strip()
 
-    url_list=[]
+    url_list = []
 
-    
-    url_list=[return_dict(song) for song in songlist]
+    url_list = [return_dict(song) for song in songlist]
 
     return jsonify(url_list)
 
+
 if __name__ == '__main__':
     app.run()
-
-
-
-   
-
