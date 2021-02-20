9832c025b76f1a3fa34b6c268f6c0bbf512f45b4
aditya-purang <44022838+aditya-purang@users.noreply.github.com>
2021-02-20 17:35:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ title: "Ashes on Fire",
 def return_dict(song_name):
 
     song_dict = CustomSearch(
-        song_name, VideoSortOrder.viewCount, limit=1).result()
+        song_name, VideoSortOrder.relevance, limit=1).result()
     src = song_dict.get("result")[0].get("link")
     image_src = song_dict.get("result")[0].get("thumbnails")[0].get("url")
     if(len(song_dict.get("result")[0].get("thumbnails"))>1):
